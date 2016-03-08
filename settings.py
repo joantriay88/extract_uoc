8297a7df86e4e9e835e73a63363ca4c02d3c8dc1
joantriay88 <triayjoan@gmail.com>
2016-03-08 12:38:41 +0100

    I add this file for the initial configutation. Only Needs to be changed JSON_PATH and API_KEY

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+
+
+# PATH SETTINGS:
+JSON_PATH = "THE_PATH_OF_YOUR_JSON_FILE_TRACKING_LOG"
+ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+TEXT_FILES_PATH = "dataExtracted/files/"
+TSVS_HEATMAP_FILES = "dataExtracted/visual/heatmaps/tsvs/"
+HEATMAP_FILES = "dataExtracted/visual/heatmaps/"
+
+# YOUTUBE V3 API KEY
+API_KEY = "YOUR_API_V3_YOUTUBE_KEY"
